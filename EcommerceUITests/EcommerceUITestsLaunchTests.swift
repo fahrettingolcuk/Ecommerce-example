9c6853f57f99c53a475004ee4a2bb1bd8180bc57
Fahrettin Gölcük <fahrettin.golcuk0@gmail.com>
2024-02-28 13:43:05 +0300

    Initial Commit

---
+++ EcommerceUITests/EcommerceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EcommerceUITestsLaunchTests.swift
+//  EcommerceUITests
+//
+//  Created by Fahrettin Gölcük on 28.02.2024.
+//
+
+import XCTest
+
+final class EcommerceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
